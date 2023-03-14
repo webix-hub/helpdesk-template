df3b03d95ae9147488fdf5157444b9a426248b54
Mariya Demy <Mariya.Demi373@gmail.com>
2023-03-14 11:19:11 +0300

    [update] webpack5 config and dependencies

--- build/webpack.dev.config.js
+++ build/webpack.dev.config.js
@@ -1,18 +1,20 @@
 const webpack = require("webpack");
-const merge = require("webpack-merge");
+const { merge } = require("webpack-merge");
 const baseWebpackConfig = require("./webpack.base.config");
 
 const devWebpackConfig = merge(baseWebpackConfig, {
 	mode: "development",
 
-	devtool: "cheap-module-eval-source-map",
+	devtool: "eval-cheap-module-source-map",
 
 	devServer: {
-		contentBase: baseWebpackConfig.externals.paths.dist,
+		static: baseWebpackConfig.externals.paths.dist,
 		port: 8080,
-		overlay: {
-			warnings: false,
-			errors: true
+		client: {
+			overlay: {
+				errors: true,
+				warnings: false,
+			},
 		}
 	},
 
